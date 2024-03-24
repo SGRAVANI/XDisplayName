69800a96e7e25f6230f5fbd914cb0f0c721fa568
SGRAVANI <shakuntla.ravani@gmail.com>
2024-03-25 00:18:45 +0530

    Test commit

--- src/Component/Child.js
+++ src/Component/Child.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import styles from "./style.module.css"
 export default function Child(props) {
+    console.log(props.data)
   return (
     <div className={styles.childContainer}>
         <p>Full Name: {props.data.fname} {props.data.lname}</p>
-        Child</div>
+       </div>
   )
 }
--- src/Component/Parent.js
+++ src/Component/Parent.js
@@ -4,21 +4,28 @@ import Child from './Child';
 export default function Parent() {
     let [ip,setIp]=useState({fname:"",lname:""})
     let [f,setF]=useState(false)
+    let [sbmtData,setSbmtData]=useState({})
     function handleSubmit(e)
     {
     e.preventDefault();
     setF(true)
+    //data={fname:ip.fname,lname:ip.lname}
+    setSbmtData(ip)
+
     console.log("data is submitted")
     }
     function handleChange(e){
         setIp((prev)=>{
             return {...prev,[e.target.name]:e.target.value}
         })
+        
     }
 
   return (
     <div>
+      
     <form className={styles.container} onSubmit={handleSubmit}>
+    <h1>Full Name Display</h1>
     <div className={styles.inputContainer}>
         <label htmlFor='fname'> First Name:</label>
         <input type="text" required name="fname" id="fname" onChange={handleChange} value={ip.fname}/>
@@ -29,12 +36,12 @@ export default function Parent() {
         <input type="text" required name="lname" id="lname" onChange={handleChange} value={ip.lname}/>
 
     </div>
-    <div className={styles.btnContainer}>
+    <div className={styles.buttonContainer}>
         <button type="submit" >Submit</button>
     </div>
     </form>
     
-   {f&& <Child data={ip} />}        
+   {f&& <Child data={sbmtData} />}        
     </div>
   )
 }
